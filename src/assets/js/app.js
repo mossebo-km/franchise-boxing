078642e29e6a64faf51f7b0d8c843122cc8805d5
Vlad Starkovsky <vlad@starkovsky.ru>
2018-01-31 20:40:35 +0300

    31.01.2018 #update

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -23,10 +23,31 @@ import $ from 'jquery';
 
 $(document).ready(function () {
 
-    $('.block-video .button').click(function () {
-        $('.block-video__form').css('display', 'flex');
-        $('.block-video__desc').css('display', 'none');
+    // Плавная прокрутка к якорю
+    $(".nav a").bind("click", function(e){
+        var anchor = $(this);
+        var name = anchor.attr("href").replace(new RegExp("#", "gi"), "");
+        $('html, body').stop().animate({
+            scrollTop: $("a[name=" + name + "]").offset().top
+        }, 700);
+        e.preventDefault();
+        return false;
     });
 
 
+    // Форма в шапке
+    $('.block-video .button').click(function () {
+        $('.block-video__form').css('display', 'flex').show(100);
+        $('.block-video__desc').hide(100);
+    });
+
+    // Высчитывание ширины фоток в блоке репутация
+    var DeviceWidth = $(window).width();
+    $('.brand-photo-wrap').css('width', DeviceWidth).css('left',(-DeviceWidth/2));
+    $('.brand-photo-item_b').css('width',(DeviceWidth-40)/10*2.33333);
+    $('.brand-photo-item_s').css('width',(DeviceWidth-40)/10*1.5);
+
+
+
 });
+
